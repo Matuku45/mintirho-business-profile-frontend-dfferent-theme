a3062dc3ee06858c25047e4934f40c03d649e8fa
Thabiso Mapoulo <manuelthabisomapoulo@gmail.com>
2025-12-03 21:35:08 +0200

    introducing reacter dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 
 import Home from './pages/Home'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+
+
+
 
 function App() {
   
@@ -8,7 +14,9 @@ function App() {
   return (
     <>
       <div>
+        <Header />
         <Home />
+        <Footer />
      </div>
     </>
   )
